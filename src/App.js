f755cff850cf5d2eedfcbc7e2d867e73a24bfe95
Ali Dogan <alidogn98@gmail.com>
2022-09-07 18:43:21 +0300

    added map to json

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import logo from "./logo.svg";
 import "./App.css";
+import { useState } from "react";
 import React, { Component } from "react";
-import Fcomponent from "./Fcomponent";
 import ForecastDetails from "./components/ForecastDetails/ForecastDetails";
 import ForecastSummaries from "./components/ForecastSummaries/ForecastSummaries";
 import ForecastSummary from "./components/ForecastSummary/ForecastSummary";
 import LocationDetails from "./components/LocationDetails/LocationDetails";
-import { useState } from "react";
-import { location } from './Weatherdata.json'
+import Data from "./Weatherdata.json";
 
 function App() {
+  console.log(Data.location.city);
+  console.log(Data.forecasts[0].date);
+
+  const location = { city: Data.location.city, country: Data.location.country };
+  const date = {time: Data.forecasts.map((forecast) => {
+    return forecast.date;
+  })};
+  console.log(date);
+
   return (
     <div>
-      <h1>test</h1>
-      <LocationDetails/>
+      <LocationDetails location={location.city} country={location.country} />
+      <div>
+        {Data.forecasts.map((forecast) => {
+          return <div>{forecast.wind.speed}</div>;
+        })}
+      </div>
+      <div>
+        <ForecastSummaries />
+      </div>
+      <div>
+        <ForecastDetails date = {date}/>
+      </div>
     </div>
   );
 }
--- src/Fcomponent.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Fcomponent() {
-  return (
-    <div>
-      selam aleykum ak
-      Fcomponentasdasdasd</div>
-  )
-}
