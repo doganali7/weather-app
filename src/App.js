8c536e0b84aa2b464f45f42a874b79247dc6c938
Ali Dogan <alidogn98@gmail.com>
2022-09-15 04:33:38 +0300

    restructuring the project

--- src/App.js
+++ src/App.js
@@ -1,37 +1,11 @@
-import "./App.css";
-import { useState } from "react";
-import React, { Component } from "react";
-import ForecastDetails from "./components/ForecastDetails/ForecastDetails";
-import ForecastSummaries from "./components/ForecastSummaries/ForecastSummaries";
-import ForecastSummary from "./components/ForecastSummary/ForecastSummary";
-import LocationDetails from "./components/LocationDetails/LocationDetails";
-import Data from "./Weatherdata.json";
+import './App.css';
 
 function App() {
-  console.log(Data.location.city);
-  console.log(Data.forecasts[0].date);
-
-  const location = { city: Data.location.city, country: Data.location.country };
-  const date = {time: Data.forecasts.map((forecast) => {
-    return forecast.date;
-  })};
-  console.log(date);
-
   return (
-    <div>
-      <LocationDetails location={location.city} country={location.country} />
-      <div>
-        {Data.forecasts.map((forecast) => {
-          return <div>{forecast.wind.speed}</div>;
-        })}
-      </div>
-      <div>
-        <ForecastSummaries />
-      </div>
-      <div>
-        <ForecastDetails date = {date}/>
-      </div>
+    <div className="App">
+      <h1>Weather App</h1>
     </div>
   );
 }
+
 export default App;
